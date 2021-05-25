cefc8c9e9feffb298e06231da12b2c5ffb6a8ed0
Amal070146 <83328946+Amal070146@users.noreply.github.com>
2021-05-25 10:52:21 +0530

    Delete scmapp directory

--- scmapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import User, Admin, Event, Book_ground,Admission
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Admin)
-admin.site.register(Event)
-admin.site.register(Book_ground)
-admin.site.register(Admission)
